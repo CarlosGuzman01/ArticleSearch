6d2777e61d3bd8b38290249fdbc2f95258a0a10d
CarlosGuzman01 <carlosguzmantejeda13@gmail.com>
2024-03-25 10:49:20 -0400

    Initial commit

---
+++ app/src/main/java/com/codepath/articlesearch/Article.kt
@@ -0,0 +1,45 @@
+package com.codepath.articlesearch
+
+import android.support.annotation.Keep
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Keep
+@Serializable
+data class SearchNewsResponse(
+    @SerialName("response")
+    val response: BaseResponse?
+)
+
+@Keep
+@Serializable
+data class BaseResponse(
+    @SerialName("docs")
+    val docs: List<Article>?
+)
+
+
+@Keep
+@Serializable
+data class Article(
+    @SerialName("abstract")
+    val abstract: String?,
+
+    @SerialName("byline")
+    val byline: Byline?,
+
+    @SerialName("headline")
+    val headLine: Headline?,
+
+    @SerialName("multimedia")
+    val multimedia: List<MultiMedia>?,
+
+    ) : java.io.Serializable {
+    val mediaImageUrl = "https://www.nytimes.com/${multimedia?.firstOrNull { it.url != null }?.url ?: ""}"
+}
+
+
+
+
+
